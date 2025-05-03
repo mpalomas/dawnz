dbe0c0d690fe894ecbbafb60320e9c55cc72b89a
Michaël Palomas <mpalomas@proton.me>
2025-05-03 12:50:26 +0200

    cleanup build.zig

--- build.zig
+++ build.zig
@@ -1,29 +1,12 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
     const target_os_tag = target.result.os.tag;
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
-    const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+    const dawn_mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
@@ -50,81 +33,30 @@ pub fn build(b: *std.Build) void {
         else => unreachable,
     };
 
-    // We will also create a module for our other entry point, 'main.zig'.
-    // const exe_mod = b.createModule(.{
-    //     // `root_source_file` is the Zig "entry point" of the module. If a module
-    //     // only contains e.g. external object files, you can make this `null`.
-    //     // In this case the main source file is merely a path, however, in more
-    //     // complicated build scripts, this could be a generated file.
-    //     .root_source_file = b.path("src/main.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
-    // exe_mod.addImport("dawnz_lib", lib_mod);
-
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
-    const lib = b.addLibrary(.{
+    const dawn_lib = b.addLibrary(.{
         .linkage = .static,
         .name = "dawnz",
-        .root_module = lib_mod,
+        .root_module = dawn_mod,
     });
-    lib.addIncludePath(b.path("dawn/include"));
-    lib.linkLibC();
-    lib.installHeadersDirectory(b.path("dawn/include/dawn"), "dawn", .{});
-    lib.addLibraryPath(b.path(dawn_lib_path));
-    lib.linkSystemLibrary("dawn");
+    dawn_lib.addIncludePath(b.path("dawn/include"));
+    dawn_lib.linkLibC();
+    dawn_lib.installHeadersDirectory(b.path("dawn/include/dawn"), "dawn", .{});
+    dawn_lib.addLibraryPath(b.path(dawn_lib_path));
+    dawn_lib.linkSystemLibrary("dawn");
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installBinFile(dawn_path, dawn_lib_name);
-    b.installArtifact(lib);
+    // b.installArtifact(lib);
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
-    // const exe = b.addExecutable(.{
-    //     .name = "dawnz",
-    //     .root_module = exe_mod,
-    // });
+    const install_dawn_lib = b.addInstallArtifact(dawn_lib, .{});
+    const install_dawn = b.step("install_dawn", "Install Dawn");
+    install_dawn.dependOn(&install_dawn_lib.step);
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    // b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    // const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    // run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    // if (b.args) |args| {
-    //     run_cmd.addArgs(args);
-    // }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    // const run_step = b.step("run", "Run the app");
-    // run_step.dependOn(&run_cmd.step);
+    b.getInstallStep().dependOn(&install_dawn_lib.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+        .root_module = dawn_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
